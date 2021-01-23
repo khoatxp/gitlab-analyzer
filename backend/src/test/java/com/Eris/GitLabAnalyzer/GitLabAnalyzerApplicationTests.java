5f290f38c791b44169d07bb17caaa08e8ff7ea72
Asim Shrestha <ashresth@sfu.ca>
2021-01-23 12:12:09 -0800

    feat: import backend

---
+++ backend/src/test/java/com/Eris/GitLabAnalyzer/GitLabAnalyzerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Eris.GitLabAnalyzer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitLabAnalyzerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
