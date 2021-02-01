19bbedfb16d140d052fe464b813fafcf9dc13c96
Asim Shrestha <ashresth@sfu.ca>
2021-02-01 00:01:12 -0800

    feat: add base login page

--- frontend/pages/login/index.tsx
+++ frontend/pages/login/index.tsx
@@ -1,6 +1,8 @@
-import { Box, Button, Input, TextField } from "@material-ui/core";
+import { Box, Button, Input, TextField, Typography } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
 import React from "react";
+import Image from 'next/image'
+
 
 const useStyles = makeStyles({
   backgroundGradient: {
@@ -19,27 +21,33 @@ const index = () => {
       height="100vh"
       width="100vw"
       display="flex"
-      flexDirection="column"
       justifyContent="center"
       alignItems="center"
-    >
+			>
       <Box
         className={classes.card}
         boxShadow={20}
         width="20vw"
         height="60vh"
-        display="flex"
+				display="flex"
+				flexDirection="column"
         justifyContent="center"
+				alignItems="center"
         borderRadius={16}
+				padding="25px"
       >
-        Test
-        <Box className="cardPadding">
-          <Box color="gray">
-            <Input defaultValue="Username" disableUnderline />
-          </Box>
-          <Button variant="contained">Login</Button>
+					<Image
+						src="/gitlab.png"
+        		alt="The Gitlab Logo"
+        		width={100}
+        		height={100}
+      		/>
+					<Typography variant="h6" align="center">GitLab<br/>Analyzer</Typography>
+					<TextField fullWidth placeholder="Username" variant="filled" />
+					<TextField fullWidth placeholder="Password" variant="filled" />
+					<TextField fullWidth placeholder="GitLab Token" variant="filled" />
+          <Button variant="contained" color="primary" disableElevation>Login</Button>
         </Box>
-      </Box>
     </Box>
   );
 };
