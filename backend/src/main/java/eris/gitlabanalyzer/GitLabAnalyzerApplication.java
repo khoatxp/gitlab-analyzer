b37a3a608dd0dd2fb1d1bca950165cc7fc0eb617
Asim Shrestha <ashresth@sfu.ca>
2021-01-31 17:29:50 -0800

    feat: update spring boot dependencies and app info

---
+++ backend/src/main/java/eris/gitlabanalyzer/GitLabAnalyzerApplication.java
@@ -0,0 +1,13 @@
+package eris.gitlabanalyzer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitLabAnalyzerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitLabAnalyzerApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/eris/gitlabanalyzer/GitLabAnalyzerApplicationTests.java
@@ -0,0 +1,13 @@
+package eris.gitlabanalyzer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitLabAnalyzerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
