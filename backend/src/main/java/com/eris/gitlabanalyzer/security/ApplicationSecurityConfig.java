4dcf94c3ac358f63eec7f66f23cdfb169af6a33f
Asim Shrestha <ashresth@sfu.ca>
2021-02-10 20:27:21 -0800

    feat: place cors url in app properties and remove @Cross Origin decorators

--- backend/src/main/java/com/eris/gitlabanalyzer/security/ApplicationSecurityConfig.java
+++ backend/src/main/java/com/eris/gitlabanalyzer/security/ApplicationSecurityConfig.java
@@ -1,5 +1,6 @@
 package com.eris.gitlabanalyzer.security;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -14,26 +15,28 @@ import java.util.Collections;
 @Configuration
 @EnableWebSecurity
 public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
+    @Value("${FRONTEND_URL}")
+    private String FRONTEND_URL;
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-    // TODO: Uncomment once security is fully set up. Don't want to require authentication while we're still testing routes
-    // Configures app to require authentication for every request
-//        http.cors().and()
-//                .authorizeRequests()
-//                .antMatchers("/api/v1/repo") // TODO: Remove once done testing
-//                .permitAll()
-//                .anyRequest()
-//                .authenticated()
-//                .and()
-//                .httpBasic();
+        // Configures app to require authentication for every request
+        http
+                .cors()
+                .and()
+                .authorizeRequests()
+                .antMatchers("**") // TODO: This pattern will permit all routes to skip authentication. Remove once authentication fully implemented.
+                .permitAll()
+                .anyRequest()
+                .authenticated()
+                .and()
+                .httpBasic();
     }
 
     @Bean
-    CorsConfigurationSource corsConfigurationSource()
-    {
+    CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Collections.singletonList("http://localhost:3000")); // TODO: get URL from ENV
+        configuration.setAllowedOrigins(Collections.singletonList(FRONTEND_URL));
         configuration.setAllowedMethods(Collections.singletonList("*"));
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", configuration);
