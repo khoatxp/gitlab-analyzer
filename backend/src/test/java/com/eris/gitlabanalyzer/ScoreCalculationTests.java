65f1e3d3295b2f7e360b62a6ac785643ba42a8dc
AndrewTurner <anturner@sfu.ca>
2021-02-18 12:07:39 -0800

    update tests

--- backend/src/test/java/com/eris/gitlabanalyzer/ScoreCalculationTests.java
+++ backend/src/test/java/com/eris/gitlabanalyzer/ScoreCalculationTests.java
@@ -2,28 +2,30 @@ package com.eris.gitlabanalyzer;
 
 import com.eris.gitlabanalyzer.dataprocessing.CalculateDiffScore;
 import com.eris.gitlabanalyzer.model.GitLabFileChange;
-import com.eris.gitlabanalyzer.model.GitLabMergeRequest;
 import com.eris.gitlabanalyzer.service.GitLabService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-
-import java.time.ZoneId;
-import java.time.ZonedDateTime;
+import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 class ScoreCalculationTests {
 
     @Autowired
     private GitLabService gitLabService;
-    private CalculateDiffScore calculateDiffScore = new CalculateDiffScore();
+    private final CalculateDiffScore calculateDiffScore = new CalculateDiffScore();
 
     @Test
     void check_MergeDiff() {
         long projectId = 2L;
+        // TODO modify retrieval to use database in future
         Iterable<GitLabFileChange> mr = gitLabService.getMergeRequestDiff(projectId, 3L).toIterable();
-        calculateDiffScore.calculateScore(mr);
+        assertNotNull(mr);
+        assertTrue(mr.iterator().hasNext());
+        int results = calculateDiffScore.calculateScore(mr);
+        assertTrue((results > 0));
+        System.out.println(results);
     }
 
 }
