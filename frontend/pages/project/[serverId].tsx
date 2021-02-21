5b6248ae0cd73cf327f817577e336633b9f76398
Ida Nakhostin <inakhost@sfu.ca>
2021-02-21 14:35:28 -0800

    fix: fix layout issue on prohect search page
    - prevent analyze button from clipping outside the wrapping card
    - don't show datetime picker while projects is loading
    - use our button component

--- frontend/pages/project/[serverId].tsx
+++ frontend/pages/project/[serverId].tsx
@@ -1,10 +1,11 @@
-import { Box, Button, TextField, Typography, LinearProgress } from "@material-ui/core";
-import { makeStyles } from "@material-ui/core/styles";
+import {Box, TextField, Typography, LinearProgress} from "@material-ui/core";
+import {makeStyles} from "@material-ui/core/styles";
 import React, {useState, useEffect} from "react";
 import {useRouter} from "next/router";
 import axios, {AxiosResponse} from "axios";
 import CardLayout from "../../components/CardLayout";
 import AppDateTimePicker from "../../components/AppDateTimePicker";
+import AppButton from "../../components/AppButton";
 import {GitLabProject} from "../../interfaces/GitLabProject";
 import Autocomplete from "@material-ui/lab/Autocomplete";
 
@@ -15,82 +16,74 @@ const useStyles = makeStyles((theme) => ({
 }));
 
 const LoadingBar = () => {
-  return <div>
-    <Typography variant={"body1"}>
-      Loading projects...
-    </Typography>
-    <LinearProgress />
-  </div>;
+    return <div>
+        <Typography variant={"body1"}>
+            Loading projects...
+        </Typography>
+        <LinearProgress/>
+    </div>;
 }
 
 
 const index = () => {
+    const [projects, setProjects] = useState<GitLabProject[]>([]);
+    const [isLoading, setIsLoading] = useState<boolean>(true);
+    const router = useRouter();
+    const {serverId} = router.query;
 
-  const classes = useStyles();
-  const [projects, setProjects] = useState<GitLabProject[]>([]);
-  const [isLoading, setIsLoading] = useState<boolean>(true);
-  const router = useRouter();
-  const { serverId } =  router.query;
+    useEffect(() => {
+        if (router.isReady) {
+            // TODO need to pass serverId into this call to get the correct gitlab url and access code from db
+            // when that information is available in db
+            axios
+                .get(`${process.env.NEXT_PUBLIC_API_URL}/gitlab/projects`)
+                .then((resp: AxiosResponse) => {
+                    setProjects(resp.data);
+                    setIsLoading(false);
+                });
+        }
+    }, [serverId]);
 
-  useEffect(() => {
-    if (router.isReady) {
-      // TODO need to pass serverId into this call to get the correct gitlab url and access code from db
-      // when that information is available in db
-      axios
-          .get(`${process.env.NEXT_PUBLIC_API_URL}/gitlab/projects`)
-          .then((resp: AxiosResponse) => {
-            setProjects(resp.data);
-            setIsLoading(false);
-          });
+    let loadingBar = null;
+    if (isLoading) {
+        loadingBar = <LoadingBar/>;
     }
-  }, [serverId]);
+    
+    return (
+        <CardLayout>
+            {loadingBar}
+            {!isLoading && <>
+                <Autocomplete
+                    id="project-select"
+                    options={projects}
+                    getOptionLabel={(proj) => proj.name_with_namespace}
+                    renderInput={(params) => <TextField {...params} label="Search Projects" variant="outlined"/>}
+                />
 
-  let loadingBar =  null;
-  if (isLoading) {
-    loadingBar = <LoadingBar />;
-  }
+                <Box
+                    marginLeft="5px"
+                    marginRight="5px"
+                    marginTop="10px"
+                    color="white"
+                    boxShadow={0}
+                    width="56vw"
+                    minWidth="260px"
+                    display="flex"
+                    flexDirection="column"
+                    justifyContent="column"
+                    alignItems="column"
+                >
+                    <AppDateTimePicker/>
+                </Box>
+                <Box
+                    alignSelf="center"
+                    marginTop="10px">
+                    <AppButton color="primary">Analyze</AppButton>
+                </Box>
+            </>}
+        </CardLayout>
 
-
-  return (
-      <CardLayout>
-        {loadingBar}
-        {!isLoading && <Autocomplete
-            id="project-select"
-            options={projects}
-            getOptionLabel={(proj) => proj.name_with_namespace}
-            renderInput={(params) => <TextField {...params} label="Search Projects" variant="outlined" />}
-        /> }
-
-        <div className='menu' style={{ marginTop: `30px` }} align="center">
-
-          <Box
-              marginleft="5px"
-              marginright="5px"
-              color="white"
-              boxShadow={0}
-              width="56vw"
-              height="10vh"
-              minWidth="260px"
-              minHeight="10vh"
-              display="flex"
-              flexDirection="column"
-              justifyContent="column"
-              alignItems="column"
-          >
-            <div className='Date picker'>
-              <AppDateTimePicker/>
-            </ div>
-          </Box>
-        </div>
-
-          <div align="center">
-            <Button variant="contained" color="primary" size="medium" className={classes.AnalyzeButtonMargin} >
-              ANALYZE
-            </Button>
-          </div>
-
-      </CardLayout>
-
-  );
-};
+    );
+}
+;
 export default index;
