710fee95f58ff30f230481ac742b432fc412cde4
Michael Zheng <mdzheng@sfu.ca>
2021-03-03 01:13:42 -0800

    refactor: remove unnecessary Java types

--- backend/src/main/java/com/eris/gitlabanalyzer/model/gitlabresponse/GitLabIssueNote.java
+++ backend/src/main/java/com/eris/gitlabanalyzer/model/gitlabresponse/GitLabIssueNote.java
@@ -10,19 +10,11 @@ public class GitLabIssueNote {
     private Long id;
     private String type;
     private String body;
-    private String attachment;
     private Author author;
     @JsonProperty("created_at")
     private OffsetDateTime createdAt;
-    @JsonProperty("updated_at")
-    private OffsetDateTime updatedAt;
-    private boolean system;
     @JsonProperty("noteable_id")
     private Long noteableId;
-    @JsonProperty("noteable_type")
-    private String noteableType;
-    private boolean resolvable;
-    private boolean confidential;
     @JsonProperty("noteable_iid")
     private Long noteableIid;
 }
