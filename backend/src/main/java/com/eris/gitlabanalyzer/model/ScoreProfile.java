4f311603bae52b2332908aa950d187cff53c8c72
ksonko <ksonko@sfu.ca>
2021-03-05 17:21:09 -0800

    removed extra whitespace

--- backend/src/main/java/com/eris/gitlabanalyzer/model/ScoreProfile.java
+++ backend/src/main/java/com/eris/gitlabanalyzer/model/ScoreProfile.java
@@ -1,11 +1,9 @@
 package com.eris.gitlabanalyzer.model;
 
 import javax.persistence.*;
-
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-
 import java.util.HashMap;
 import java.util.Map;
 
