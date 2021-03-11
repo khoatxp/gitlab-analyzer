a4647b74f3e11fe09a0c9b941491214d1ce92643
Jason Lee <jason_lee_18@sfu.ca>
2021-03-10 20:48:32 -0800

    Added Query methods for UserProjectPermissionRepository

--- backend/src/main/java/com/eris/gitlabanalyzer/repository/UserProjectPermissionRepository.java
+++ backend/src/main/java/com/eris/gitlabanalyzer/repository/UserProjectPermissionRepository.java
@@ -1,7 +1,18 @@
 package com.eris.gitlabanalyzer.repository;
 
+import com.eris.gitlabanalyzer.model.Project;
+import com.eris.gitlabanalyzer.model.Server;
+import com.eris.gitlabanalyzer.model.User;
 import com.eris.gitlabanalyzer.model.UserProjectPermission;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
+
+@Repository
 public interface UserProjectPermissionRepository extends JpaRepository<UserProjectPermission, Long> {
+    List<UserProjectPermission> findUserProjectPermissionByUser(User user);
+    List<UserProjectPermission> findUserProjectPermissionByUserAndServer(User user, Server server);
+    Optional<UserProjectPermission> findUserProjectPermissionByUserAndServerAndProject(User user, Server server, Project project);
 }
