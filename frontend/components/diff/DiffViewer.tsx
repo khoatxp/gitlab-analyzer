68ef302507e02238afcdb6c7912caf4e69c1ed04
Asim Shrestha <ashresth@sfu.ca>
2021-03-16 17:35:09 -0700

    refactor: extract file diff view into separate file

--- frontend/components/diff/DiffViewer.tsx
+++ frontend/components/diff/DiffViewer.tsx
@@ -1,17 +1,12 @@
 import {FileChange} from "../../interfaces/GitLabFileChange";
 import {parseFileChangesForDiffViewer} from "./FileChangeParser";
-import {ParsedFileChange} from "../../interfaces/ParsedFileChange";
-import React, {useMemo, useState} from "react";
-import {createStyles, makeStyles} from "@material-ui/core/styles";
-import {Box, Card, Divider, IconButton, ListSubheader, Typography} from "@material-ui/core";
-import {Tokenize} from "./Tokenize";
+import React, {useState} from "react";
+import {Box, Card, Divider, ListSubheader, Typography} from "@material-ui/core";
 import LinkIcon from '@material-ui/icons/Link';
-// @ts-ignore (Doesn't have typescript types)
-import {Decoration, Diff, Hunk} from 'react-diff-view';
 import List from "@material-ui/core/List";
 import AppButton from "../app/AppButton";
-import ArrowDropDownIcon from '@material-ui/icons/ArrowDropDown';
-import ArrowRightIcon from '@material-ui/icons/ArrowRight';
+import FileDiffView from "./FileDiffView";
+
 
 type DiffViewerProps = { fileChanges: FileChange[], linkToFileChanges: string };
 const DiffViewer = ({fileChanges, linkToFileChanges}: DiffViewerProps) => {
@@ -72,64 +67,5 @@ const DiffViewer = ({fileChanges, linkToFileChanges}: DiffViewerProps) => {
         </Card>
     )
 }
-
-type FileDiffViewProps = { unified?: boolean, change: ParsedFileChange }
-const FileDiffView = ({unified, change}: FileDiffViewProps) => {
-    const styles = useStyles();
-    const [hidden, setHidden] = useState<boolean>(false);
-    const tokens = useMemo(() => Tokenize(change), [change]);   // Used for syntax highlighting
-
-    return (
-        <Card raised className={styles.fileDiff}>
-            <Typography className={styles.diffHeader}>
-                <IconButton size="small" onClick={() => setHidden(!hidden)}>
-                    { hidden ? <ArrowDropDownIcon/> : <ArrowRightIcon/> }
-                </IconButton>
-                {getFileChangeHeader(change)}
-            </Typography>
-            {
-                !hidden &&
-                <Diff viewType={ unified ? "unified" : 'split'} diffType={change.type} hunks={change.hunks} tokens={tokens}>
-                    {(hunks: any[]) =>
-                        hunks.map(hunk => [
-                            <Decoration key={'deco-' + hunk.content}>
-                                <div className={styles.hunkHeader}>{hunk.content}</div>
-                            </Decoration>,
-                            <Hunk key={hunk.content} hunk={hunk}/>,
-                        ])
-                    }
-                </Diff>
-            }
-        </Card>
-    )
-}
-
-const getFileChangeHeader = (parsedFileChange: ParsedFileChange): string => {
-    if (parsedFileChange.oldPath === parsedFileChange.newPath) {
-        return parsedFileChange.oldPath;
-    } else {
-        return `${parsedFileChange.oldPath} -> ${parsedFileChange.newPath}`
-    }
-}
-
-// Styling adapted from https://codesandbox.io/s/149oz0nw1q
-const useStyles = makeStyles(() =>
-    createStyles({
-        fileDiff: {
-            marginBottom: '1em',
-            borderRadius: '10px',
-            boxShadow: '20px',
-        },
-        diffHeader: {
-            padding: '0.75em',
-            borderBottom: '1px solid lightgray',
-        },
-        hunkHeader: {
-            backgroundColor: '#f0f8ff',
-            color: '#1b1f2380',
-            padding: '5px 20px',
-        },
-    }),
-);
 
 export default DiffViewer;
---
+++ frontend/components/diff/FileDiffView.tsx
@@ -0,0 +1,71 @@
+import {createStyles, makeStyles} from "@material-ui/core/styles";
+import {ParsedFileChange} from "../../interfaces/ParsedFileChange";
+import React, {useMemo, useState} from "react";
+import {Tokenize} from "./Tokenize";
+import {Card, IconButton, Typography} from "@material-ui/core";
+import ArrowDropDownIcon from "@material-ui/icons/ArrowDropDown";
+import ArrowRightIcon from "@material-ui/icons/ArrowRight";
+// @ts-ignore (Doesn't have typescript types)
+import {Decoration, Diff, Hunk} from 'react-diff-view';
+
+// Styling adapted from https://codesandbox.io/s/149oz0nw1q
+const useStyles = makeStyles(() =>
+    createStyles({
+        fileDiff: {
+            marginBottom: '1em',
+            borderRadius: '10px',
+            boxShadow: '20px',
+        },
+        diffHeader: {
+            padding: '0.75em',
+            borderBottom: '1px solid lightgray',
+        },
+        hunkHeader: {
+            backgroundColor: '#f0f8ff',
+            color: '#1b1f2380',
+            padding: '5px 20px',
+        },
+    }),
+);
+
+type FileDiffViewProps = { unified?: boolean, change: ParsedFileChange }
+const FileDiffView = ({unified, change}: FileDiffViewProps) => {
+    const styles = useStyles();
+    const [hidden, setHidden] = useState<boolean>(false);
+    const tokens = useMemo(() => Tokenize(change), [change]);   // Used for syntax highlighting
+
+    return (
+        <Card raised className={styles.fileDiff}>
+            <Typography className={styles.diffHeader}>
+                <IconButton size="small" onClick={() => setHidden(!hidden)}>
+                    { hidden ? <ArrowDropDownIcon/> : <ArrowRightIcon/> }
+                </IconButton>
+                {getFileChangeHeader(change)}
+            </Typography>
+
+            {
+                !hidden &&
+                <Diff viewType={ unified ? "unified" : 'split'} diffType={change.type} hunks={change.hunks} tokens={tokens}>
+                    {(hunks: any[]) =>
+                        hunks.map(hunk => [
+                            <Decoration key={'deco-' + hunk.content}>
+                                <div className={styles.hunkHeader}>{hunk.content}</div>
+                            </Decoration>,
+                            <Hunk key={hunk.content} hunk={hunk}/>,
+                        ])
+                    }
+                </Diff>
+            }
+        </Card>
+    )
+}
+
+const getFileChangeHeader = (parsedFileChange: ParsedFileChange): string => {
+    if (parsedFileChange.oldPath === parsedFileChange.newPath) {
+        return parsedFileChange.oldPath;
+    } else {
+        return `${parsedFileChange.oldPath} -> ${parsedFileChange.newPath}`
+    }
+}
+
+export default FileDiffView;
