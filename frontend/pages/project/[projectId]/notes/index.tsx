e60174845df9a010e1eb054de38d86284ed205d2
Michael Zheng <mdzheng@sfu.ca>
2021-03-18 17:27:21 -0700

    feat: remove MR/issue list from notes page

--- frontend/pages/project/[projectId]/notes/index.tsx
+++ frontend/pages/project/[projectId]/notes/index.tsx
@@ -1,8 +1,7 @@
-import {createStyles, makeStyles, Theme} from "@material-ui/core/styles";
-import React, {memo, useEffect, useState} from "react";
+import {createStyles, makeStyles} from "@material-ui/core/styles";
+import React, {useEffect, useState} from "react";
 import axios, {AxiosResponse} from "axios";
 import {
-    Box,
     Card,
     Container,
     FormControl,
@@ -16,13 +15,9 @@ import {
     RadioGroup,
     Typography
 } from "@material-ui/core";
-import {areEqual, FixedSizeList} from 'react-window';
-import memoize from 'memoize-one';
-import AutoSizer from 'react-virtualized-auto-sizer';
 import {Note} from "../../../../interfaces/GitLabNote";
 import {MergeRequest} from "../../../../interfaces/GitLabMergeRequest";
 import {Issue} from "../../../../interfaces/GitLabIssue";
-import {Task} from "../../../../interfaces/GitLabTask";
 import {useRouter} from "next/router";
 import {AuthContext} from "../../../../components/AuthContext";
 import AuthView from "../../../../components/AuthView";
@@ -30,13 +25,8 @@ import MenuLayout from "../../../../components/layout/menu/MenuLayout";
 import formatDate from "../../../../interfaces/dateFormatter";
 import {useSnackbar} from "notistack";
 
-const useStyles = makeStyles((theme: Theme) =>
+const useStyles = makeStyles(() =>
     createStyles({
-        appBarSpacer: theme.mixins.toolbar,
-        taskList: {
-            overflow: 'auto',
-            height: '75vh',
-        },
         notesList: {
             overflow: 'auto',
             height: '80vh',
@@ -59,30 +49,15 @@ const NotesPage = () => {
 
     const [noteType, setNoteType] = useState(NoteType.MergeRequest);
 
-    const [mergeRequestWordCounts, setMergeRequestWordCounts] = useState<number[]>([]);
-    const [issueWordCounts, setIssueWordCounts] = useState<number[]>([]);
-
     const {getAxiosAuthConfig} = React.useContext(AuthContext);
 
     const router = useRouter();
     const {projectId, startDateTime, endDateTime} = router.query;
     const PROJECT_ID_URL = `${process.env.NEXT_PUBLIC_API_URL}/gitlab/projects/${projectId}`;
 
-    const [selectedItem, setSelectedItem] = useState(0);
-
     const handleSelectNoteType = (event: React.ChangeEvent<HTMLInputElement>) => {
         setNoteType(Number((event.target as HTMLInputElement).value));
 
-    };
-
-    useEffect(() => {
-        handleSelectItem(0);
-    }, [noteType]);
-
-    const handleSelectItem = (
-        index: number,
-    ) => {
-        setSelectedItem(index);
     };
 
     useEffect(() => {
@@ -107,7 +82,6 @@ const NotesPage = () => {
 
     useEffect(() => {
         getAllMergeRequestNotes();
-        handleSelectItem(0); // first merge request is selected on page load
     }, [mergeRequests]);
 
     useEffect(() => {
@@ -138,60 +112,22 @@ const NotesPage = () => {
         });
     };
 
-    useEffect(() => {
-        setMergeRequestWordCounts(
-            mergeRequestNotes.map(
-                (notes) =>
-                    notes.reduce((totalWords, note) => (totalWords + getWordCount(note.body)), 0)
-            )
-        )
-    }, [mergeRequestNotes]);
-
-    useEffect(() => {
-        setIssueWordCounts(
-            issueNotes.map(
-                (notes) =>
-                    notes.reduce((totalWords, note) => (totalWords + getWordCount(note.body)), 0)
-            )
-        )
-    }, [issueNotes]);
-
-    const classes = useStyles();
     return (
         <AuthView>
             <MenuLayout tabSelected={2}>
                 <Container maxWidth='xl'>
                     <Grid container spacing={2}>
-
-                        <Grid item xs={12} md={4} lg={3}>
-                            <Card>
-                                <Box style={{display: 'flex', justifyContent: 'center'}}>
-                                    <RadioGroupSelectMergeRequestsOrIssues
-                                        value={noteType}
-                                        handleChange={handleSelectNoteType}
-                                        numMergeRequests={mergeRequests.length}
-                                        numIssues={issues.length}
-                                    />
-                                </Box>
-                                <Box className={classes.taskList}>
-                                    <TaskList items={noteType === NoteType.MergeRequest ? mergeRequests : issues}
-                                              selectedItem={selectedItem}
-                                              handleSelectedItemChange={handleSelectItem}
-                                              wordCounts={
-                                                  noteType === NoteType.MergeRequest ?
-                                                      mergeRequestWordCounts : issueWordCounts
-                                              }
-                                    />
-                                </Box>
-                            </Card>
+                        <Grid>
+                            <RadioGroupSelectMergeRequestsOrIssues
+                                value={noteType}
+                                handleChange={handleSelectNoteType}
+                            />
                         </Grid>
-
-                        <Grid item xs={12} md={8} lg={9}>
+                        <Grid item xs={12} md={10} lg={10}>
                             <Card>
-                                <NotesList notes={
+                                <NotesList noteArrays={
                                     noteType === NoteType.MergeRequest ?
-                                        mergeRequestNotes[selectedItem] :
-                                        issueNotes[selectedItem]
+                                        mergeRequestNotes : issueNotes
                                 }
                                 />
                             </Card>
@@ -203,133 +139,69 @@ const NotesPage = () => {
     );
 };
 
-const RadioGroupSelectMergeRequestsOrIssues = ({value, handleChange, numMergeRequests, numIssues}
+const RadioGroupSelectMergeRequestsOrIssues = ({value, handleChange}
                                                    : {
     value: NoteType,
     handleChange: React.Dispatch<React.ChangeEvent<HTMLInputElement>>,
-    numMergeRequests: number,
-    numIssues: number,
 }) => {
 
     return (
         <FormControl component="fieldset">
-            <RadioGroup row
-                        name="mergeRequestsOrIssues"
+            <RadioGroup name="mergeRequestsOrIssues"
                         defaultValue={NoteType.MergeRequest}
                         value={value}
                         onChange={handleChange}>
                 <FormControlLabel
                     value={NoteType.MergeRequest}
                     control={<Radio color="primary" size="small"/>}
-                    label={`${numMergeRequests} Merge Requests`}
+                    label={`Merge Requests`}
                 />
                 <FormControlLabel
                     value={NoteType.Issue}
                     control={<Radio color="secondary" size="small"/>}
-                    label={`${numIssues} Issues`}
+                    label={`Issues`}
                 />
             </RadioGroup>
         </FormControl>
     );
 }
 
-const Row = memo(({data, index, style}
-                      : {
-    data: { items: Task[], selectedItem: number, setSelectedItem: React.Dispatch<number>, wordCounts: number[] },
-    index: number,
-    style: React.CSSProperties
-}) => {
 
-    const {items, selectedItem, setSelectedItem, wordCounts} = data;
-    const item = items[index];
-
-    return (
-        <ListItem
-            button
-            onClick={() => setSelectedItem(index)}
-            selected={selectedItem === index}
-            disableRipple
-            style={style}
-        >
-            <ListItemText
-                primary={
-                    <>
-                        {`${item.title} `}
-                        <Typography
-                            component="span"
-                            variant="body2"
-                            color="textSecondary"
-                        >
-                            {`· ${wordCounts[index] ?? "..."} words`}
-                        </Typography>
-                    </>}
-                secondary={`#${item.iid} · opened ${formatDate(item.created_at)} by ${item.author.name}`}/>
-        </ListItem>
-    );
-}, areEqual);
-
-const createItemData = memoize((items: Task[],
-                                selectedItem: number,
-                                setSelectedItem: React.Dispatch<number>,
-                                wordCounts: number[],
-) => ({
-    items,
-    selectedItem,
-    setSelectedItem,
-    wordCounts,
-}));
-
-const TaskList = ({items, selectedItem, handleSelectedItemChange, wordCounts}
-                      : {
-                      items: Task[],
-                      selectedItem: number,
-                      handleSelectedItemChange: React.Dispatch<number>,
-                      wordCounts: number[],
-                  }
-) => {
-
-    const itemData = createItemData(items, selectedItem, handleSelectedItemChange, wordCounts);
-    return (
-        <AutoSizer>
-            {({height, width}) => (
-                <FixedSizeList
-                    height={height}
-                    itemCount={items.length}
-                    itemData={itemData}
-                    itemSize={120}
-                    width={width}
-                >
-                    {Row}
-                </FixedSizeList>
-            )}
-        </AutoSizer>
-    );
-}
-
-const NotesList = ({notes}: { notes: Note[] }) => {
+const NotesList = ({noteArrays}: { noteArrays: Note[][] }) => {
     const classes = useStyles();
 
     return (
-        <List subheader={<ListSubheader disableSticky>{`${notes?.length ?? "..."} Notes`}</ListSubheader>}
+        <List subheader={<ListSubheader disableSticky>{`Notes`}</ListSubheader>}
               className={classes.notesList}
         >
-            {notes?.map((note) => (
-                <ListItem key={note.id}>
-                    <ListItemText
-                        primary={
-                            <>
-                                {`${note.author.name} `}
+            {noteArrays?.map((noteArray) => (
+                noteArray.map((note) => (
+                    <ListItem key={note.id}>
+                        <ListItemText
+                            primary={
+                                <>
+                                    {`${note.author.name} `}
+                                    <Typography
+                                        component="span"
+                                        variant="body2"
+                                        color="textSecondary"
+                                    >
+                                        {`@${note.author.username} · ${formatDate(note.created_at)} · ${getWordCount(note.body)} words`}
+                                    </Typography>
+                                </>}
+                            secondary={
+                                <>
                                 <Typography
                                     component="span"
                                     variant="body2"
-                                    color="textSecondary"
+                                    color="textPrimary"
                                 >
-                                    {`@${note.author.username} · ${formatDate(note.created_at)} · ${getWordCount(note.body)} words`}
+                                    {note.body}
                                 </Typography>
                             </>}
-                        secondary={note.body}/>
-                </ListItem>
-            ))}
+                        />
+                    </ListItem>
+                ))))}
         </List>
     );
 };
