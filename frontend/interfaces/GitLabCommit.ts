cb322399dfae931dcb7a0937255d08ff5b57bc34
Asim Shrestha <ashresth@sfu.ca>
2021-04-04 00:34:55 -0700

    refactor: removed unused gitlab commit file

--- frontend/interfaces/GitLabCommit.ts
+++
@@ -1,9 +0,0 @@
-export interface Commit {
-    author_email: string;
-    author_name: string;
-    created_at: string;
-    id: string;
-    sha: string;
-    title: string;
-    web_url: string;
-}
--- frontend/interfaces/GitLabMergeRequest.ts
+++ frontend/interfaces/GitLabMergeRequest.ts
@@ -6,19 +6,3 @@ export interface MergeRequest extends Task {
     sha: string;
     merge_commit_sha: null | string;
 }
-
-
-/*
- * The merge request list item in the code page that
- * displays orphan commits on click.
- *
- * Treated as a "Merge Request" so that it can be easily supplied to the list
- * and rendered
- */
-export const OrphanCommitMergeRequest = {
-    id: 0,
-    createdAt: '',
-    author: {name: ''},
-    title: 'Orphan Commits',
-    secondaryText: 'Commits pushed directly to the main branch'
-} as unknown as MergeRequest & { secondaryText: string }
